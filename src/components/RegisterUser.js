521f5865e019c51bbf10a5384194aaca4b351853
kidaniland <mdcvillas@gmail.com>
2021-06-30 15:28:41 -0400

    Se crea elemento Formulario del registro y tema de colores para la app

--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Title, ContainerButtons, ContainerHeader } from '../elements/Header';
+import Button from '../elements/Button';
+import { 
+    Form, 
+    Input, 
+    ContainerBtn } from '../elements/FormElements';
+import { ReactComponent as SvgLogin } from '../images/registro.svg';
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 6.25rem;
+    margin-bottom: 1.25rem;
+`; 
 
 const RegisterUser = () => {
     return (
-        <h1>
-           Registro de usuario 
-        </h1>
+        <>
+            <Helmet>
+                <title>Crear cuentas</title>
+            </Helmet> 
+            <Header>
+                <ContainerHeader>
+                    <Title>Crear cuenta</Title>
+                    <div>
+                        <Button to="/iniciar-sesion">Iniciar Seseión</Button>
+                    </div>
+
+                </ContainerHeader>
+            </Header>
+            <Form >
+                <Svg/>
+                <Input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Correo Electrónico"
+                />
+                <Input 
+                    type="password" 
+                    name="password" 
+                    placeholder="Contraseña"
+                />
+                <Input 
+                    type="password" 
+                    name="password2" 
+                    placeholder="Repetir Contraseña"
+                />
+                <ContainerBtn>
+                    <Button as="button" type="submit" primario>Crear Cuenta</Button>
+                </ContainerBtn>
+            </Form>
+        </>
     )
 }
 
